5d4efc974c1608cb9ee868e3c0c4a49a6c7db226
Daniel Havlik <dh@gocept.com>
2014-02-14 22:00:10 +0100

    Variante mit Interrupts
    Optional ;)

--- src/spacemaster.py
+++ src/spacemaster.py
@@ -6,22 +6,24 @@ context = zmq.Context()
 publisher = context.socket (zmq.PUB)
 publisher.bind ("tcp://*:9000")
 
+
+def switch_fall():
+    print("switch on")
+    publisher.send_json(dict(spaceopen=True))
+
+def switch_rise():
+    print("switch off")
+    publisher.send_json(dict(spaceopen=False))
+
+
+GPIO.cleanup()
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-message = dict(spaceopen = None)
+GPIO.add_event_detect(7, GPIO.FALLING, callback=switch_fall, bouncetime=150)  
+GPIO.add_event_detect(7, GPIO.RISING, callback=switch_rise, bouncetime=150)  
+
 
 while True:
-    try:
-        GPIO.wait_for_edge(7, GPIO.FALLING)
-        print("switch on")
-        message['spaceopen'] = True
-        publisher.send_json(message)
-        time.sleep(0.1)
-        GPIO.wait_for_edge(7, GPIO.RISING)
-        print("switch off")
-        message['spaceopen'] = False
-        publisher.send_json(message)
-        time.sleep(0.1)
-    except KeyBoardInterrupt:
-        GPIO.cleanup()
+    time.sleep(1)
+    
 GPIO.cleanup()
