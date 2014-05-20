4e223ac20a6aecbc4ffc1ebf368b1f4ce9eded38
Stefan <stefan@rabota>
2014-05-20 22:32:52 +0200

    fix interrupted based solution

--- src/spacemaster.py
+++ src/spacemaster.py
@@ -6,24 +6,29 @@ context = zmq.Context()
 publisher = context.socket (zmq.PUB)
 publisher.bind ("tcp://*:9000")
 
+channel = 7
 
-def switch_fall():
+def switch_fall(channel):
+    GPIO.remove_event_detect(channel)
+    GPIO.add_event_detect(channel, GPIO.RISING, callback=switch_rise, bouncetime=150)  
     print("switch on")
     publisher.send_json(dict(spaceopen=True))
 
-def switch_rise():
+def switch_rise(channel):
+    GPIO.remove_event_detect(channel)
+    GPIO.add_event_detect(channel, GPIO.FALLING, callback=switch_fall, bouncetime=150)  
     print("switch off")
     publisher.send_json(dict(spaceopen=False))
 
 
 GPIO.cleanup()
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(7, GPIO.FALLING, callback=switch_fall, bouncetime=150)  
-GPIO.add_event_detect(7, GPIO.RISING, callback=switch_rise, bouncetime=150)  
+GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(channel, GPIO.FALLING, callback=switch_fall, bouncetime=150)  
 
 
 while True:
-    time.sleep(1)
-    
+    time.sleep(600)
+
 GPIO.cleanup()
+
